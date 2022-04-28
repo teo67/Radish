9a8f6dfe2afab5da534e9d700a4d82e2447af71c
teo67 <teogerst@gmail.com>
2022-04-27 22:39:27 -0700

    automated production w/ windows installer exe

--- build.bat
+++ build.bat
@@ -1,14 +1,6 @@
 @echo off
 :: build your own radish
 setlocal enabledelayedexpansion
-mkdir build
-FOR %%N IN (win-x64 osx-x64) DO (
-    dotnet publish -c Release -r %%N --self-contained false -o build/%%N/Radish
-    cd build
-    cd %%N
-    :: we need to cd into the folder so 7zip doesnt zip up the surrounding folders as well
-    7z a -tzip %%N.zip Radish
-    cd ../..
-    rmdir %cd%\build\%%N\Radish /s /q
-)
+mkdir Radish
+dotnet publish -c Release -r %1 --self-contained false -o Radishs
 endlocal
